18df570d21630a9e5b5f7a7c4176ab43dbc23552
CChenyu <54934297+ruziniuuuuu@users.noreply.github.com>
2025-01-16 04:47:15 +0000

    update anaconda3 support

--- operators/check_op.py
+++ operators/check_op.py
@@ -1,7 +1,20 @@
-# TODO: define a check operator to check whether the scene data is valid
 import bpy
-import subprocess  # Import subprocess to run system commands
+import subprocess
 import os
+
+def find_conda_path():
+    """Try to find the conda executable in common locations."""
+    conda_paths = [
+        os.path.expanduser("~/miniconda3/bin/conda"),  # Linux/MacOS Miniconda
+        os.path.expanduser("~/anaconda3/bin/conda"),   # Linux/MacOS Anaconda
+        os.path.expanduser(r"~/miniconda3/Scripts/conda.exe"),  # Windows Miniconda
+        os.path.expanduser(r"~/anaconda3/Scripts/conda.exe"),   # Windows Anaconda
+    ]
+    
+    for path in conda_paths:
+        if os.path.exists(path):
+            return path
+    return None
 
 class FLOORPLAN_OT_check(bpy.types.Operator):
     """Check the scene data"""
@@ -20,10 +33,10 @@ class FLOORPLAN_OT_check(bpy.types.Operator):
                 return {'CANCELLED'}
 
             # 获取conda路径
-            if os.name == 'nt':  # Windows
-                conda_path = os.path.expanduser(r"~/miniconda3/Scripts/conda.exe")
-            else:  # Linux/MacOS
-                conda_path = os.path.expanduser("~/miniconda3/bin/conda")
+            conda_path = find_conda_path()
+            if not conda_path:
+                self.report({'ERROR'}, "未找到Conda路径，请确保Conda已安装")
+                return {'CANCELLED'}
 
             # 运行检查命令
             process = subprocess.Popen(
@@ -69,10 +82,11 @@ class FLOORPLAN_OT_demo(bpy.types.Operator):
             
             layout_path = bpy.context.scene.get('exported_yaml')
 
-            if os.name == 'nt':  # Windows
-                conda_path = os.path.expanduser(r"~/miniconda3/Scripts/conda.exe")
-            else:  # Linux/MacOS
-                conda_path = os.path.expanduser("~/miniconda3/bin/conda")
+            # 获取conda路径
+            conda_path = find_conda_path()
+            if not conda_path:
+                self.report({'ERROR'}, "未找到Conda路径，请确保Conda已安装")
+                return {'CANCELLED'}
 
             process = subprocess.Popen(
                 [conda_path, 'run', '-n', 'robocasa', 'python', python_script_path, layout_path, '--demo'],
@@ -93,5 +107,3 @@ class FLOORPLAN_OT_demo(bpy.types.Operator):
             return {'CANCELLED'}
             
         return {'FINISHED'}
-
-
